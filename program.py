f46dbfdb3a9c15f397bd13cceed7cd75cc759139
eyal1izhaki <eyal1izhaki@gmail.com>
2021-10-03 16:19:32 +0300

    Added is_prime function

--- program.py
+++ program.py
@@ -1,8 +1,15 @@
 def sum_1_100():
     return sum(range(1, 101))
 
-def main():
-    pass
 
+def is_prime(num):
+    for i in range(2, int(num/2)+1):
+        if num%i == 0:
+            return False
+    return True
+
+def main():
+    for i in [5,6,7,14,152,60693]:
+        print(f"Is {i} prime?", is_prime(i))
 if __name__ == "__main__":
     main()
