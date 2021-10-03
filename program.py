c9bb1c3ccecdcc2173d7c761ffa719f275c793fd
eyal1izhaki <eyal1izhaki@gmail.com>
2021-10-03 16:14:31 +0300

    Added factorial function

--- program.py
+++ program.py
@@ -1,8 +1,18 @@
 def sum_1_100():
     return sum(range(1, 101))
 
+
+def factorial(num):
+    result = 1
+    for i in range(1, num+1):
+        result *= i
+    return result
+
+
 def main():
-    pass
+    for i in range(5, 9):
+        print(str(i) + "!:", factorial(i))
+
 
 if __name__ == "__main__":
     main()
