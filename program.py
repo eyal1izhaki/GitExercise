2201d128f069802f539a5c2b5815ad8752702d75
eyal1izhaki <eyal1izhaki@gmail.com>
2021-10-03 16:39:30 +0300

    .

--- program.py
+++ program.py
@@ -4,12 +4,15 @@ def sum_1_100():
 
 def is_prime(num):
     for i in range(2, int(num/2)+1):
-        if num%i == 0:
+        if num % i == 0:
             return False
     return True
 
+
 def main():
-    for i in [5,6,7,14,152,60693]:
+    for i in [5, 6, 7, 14, 152, 60693]:
         print(f"Is {i} prime?", is_prime(i))
+
+
 if __name__ == "__main__":
     main()
